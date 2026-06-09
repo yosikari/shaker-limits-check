e6de68acc4cce666bd72524caf466c1f00886913
Yossi Karasik <yosikari@gmail.com>
2026-06-09 13:04:58 +0300

    Create extract-table.js

---
+++ extract-table.js
@@ -0,0 +1,14 @@
+{
+  "name": "lds-vibration-calculator",
+  "version": "1.0.0",
+  "type": "module",
+  "scripts": {
+    "start": "vercel dev"
+  },
+  "dependencies": {
+    "openai": "latest"
+  },
+  "devDependencies": {
+    "vercel": "latest"
+  }
+}
